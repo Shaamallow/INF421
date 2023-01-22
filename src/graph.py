a13ca53898cdc867916030379e13d8879075d11a
Shaamallow <eyal.benaroche@gmail.com>
2023-01-22 18:49:53 +0100

    Working MST

--- src/graph.py
+++ src/graph.py
@@ -2,72 +2,11 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import os 
 
 # DOC :
 #[LINK](https://networkx.org/documentation/stable/tutorial.html)
-  
-# Define a Graph Class for vizualisation 
-class GraphVisualization:
-   
-    def __init__(self):
-        # Graph Object from networkx
-        self.G = nx.Graph()
-          
-        # List of edges
-        self.edges = []
 
-        # Union Find Structure for nodes
-        self.nodes = []
-          
-    # addEdge function inputs the vertices of an
-    # edge and appends it to the visual list
-    def addEdge(self, a, b,weight=None, color= None):
-
-        # Add edge to the graph
-        self.G.add_edge(a,b)
-
-        # Add edge to the list
-        self.edges.append(Edge(a,b,weight,color))
-    
-        # Add 2 Nodes with no parenting
-            # Check if the node already exist
-        a_in = False
-        b_in = False
-        for node in self.nodes:
-            if node.ID == a:
-                a_in = True
-            if node.ID == b:
-                b_in = True
-
-        if not a_in:
-            self.nodes.append(Node(a))
-        if not b_in:
-            self.nodes.append(Node(b))
-          
-    # visualize function draws the graph with matplotlib
-    def visualize(self):
-        nx.draw_networkx(self.G)
-        plt.show()
-
-    # add a single Node to the graph and update Union Find Structure
-    def addNode(self, node):
-        """
-        Add a sigle node to the graph
-        """
-
-        self.G.add_node(node)
-        # Update Union Find Structure
-        self.nodes.append(Node(node))
-
-    # Return all nodes of the graph
-    def getNodes(self):
-        """
-        Return all nodes of the graph
-        """
-        return self.G.nodes()
-
-
-# Define a Node Class for Union Find Structure
 
 class Node:
 
@@ -92,9 +31,10 @@ class Node:
     def __str__(self):
         return 'Node ' + str(self.ID)
 
+# Define an Edge Class 
 class Edge():
 
-    def __init__(self, a, b, weight=None, color=None):
+    def __init__(self, a : Node, b : Node, weight=None, color=None):
         self.a = a
         self.b = b
         self.weight = weight
@@ -103,9 +43,100 @@ class Edge():
     def __str__(self):
         return 'Edge : ' + str(self.a) + ' - ' + str(self.b) + ' | weight : ' + str(self.weight) + ' | color : ' + str(self.color)
 
+  
+# Define a Graph Class for vizualisation 
+class GraphVisualization:
+   
+    def __init__(self):
+        # Graph Object from networkx
+        self.G = nx.Graph()
+          
+        # List of edges
+        self.edges = []
+
+        # Union Find Structure for nodes
+        self.nodes = []
+
+    # add a single Node to the graph and update Union Find Structure
+    def addNode(self, nodeA : Node):
+        """
+        Add a sigle node to the graph
+        """
+        # Add node to the graph
+        self.G.add_node(nodeA.ID)
+        # Update Union Find Structure
+        self.nodes.append(nodeA)
+          
+    # addEdge function inputs the vertices of an
+    # edge and appends it to the visual list
+    def addEdge(self, a : Node, b : Node, weight=None, color= None):
+        """
+        Add an edge to the graph
+        Create a new edge if not already exist
+        Automatically add Nodes if necessary
+        """
+
+        # Add edge to the graph
+        self.G.add_edge(a.ID,b.ID)
+
+        # Add edge to the list
+        self.edges.append(Edge(a,b,weight,color))
+    
+        # Add 2 Nodes with no parenting
+            # Check if the node already exist
+
+        a_in = False
+        b_in = False
+        for node in self.nodes:
+            if node.ID == a.ID:
+                a_in = True
+            if node.ID == b.ID:
+                b_in = True
+
+        if not a_in:
+            self.nodes.append(a)
+        if not b_in:
+            self.nodes.append(b)
+          
+    # visualize function draws the graph with matplotlib
+    def visualize(self):
+        nx.draw_networkx(self.G)
+        plt.show()
+
+    # Return all nodes of the graph
+    def getNodes(self):
+        """
+        Return all nodes of the graph
+        """
+        return self.G.nodes()
+
+    # Method to get the MST of the graph
+    def Kruskal(self):
+        """
+        Return the MST of the graph
+        """
+        
+        # Sort edges by weight
+        self.edges.sort(key=lambda x: x.weight)
+
+        # Create a new Graph
+        MST = GraphVisualization()
+
+        # Add Nodes
+        for node in self.nodes:
+            MST.addNode(node)
+
+        # Add Edges
+        for edge in self.edges:
+            if edge.a.find() != edge.b.find():
+                MST.addEdge(edge.a, edge.b, edge.weight, edge.color)
+                edge.a.union(edge.b)
+
+        return MST
+
 
 # Driver code
-Graph = GraphVisualization()
+""" Graph = GraphVisualization()
 Graph.addEdge(0, 2)
 Graph.addNode(5)
 Graph.addNode(6)
@@ -132,4 +163,54 @@ NodeD.union(NodeC)
 print(NodeB.find().ID)
 print(NodeC.find().ID)
 NodeC.union(NodeD)
-print(NodeD.find().ID)
+print(NodeD.find().ID) """
+
+# RUN TEST on  ./tests/tests/itinerariesX.in
+# with X from 0 to 9
+path = os.getcwd()
+# Open test file 
+ID = 1
+file = open('tests/tests/itineraries.'+ str(ID) +'.in', 'r')
+lines = file.readlines()
+
+entry = []
+for line in lines:
+    entry.append(line.strip().split())
+
+# Close file 
+file.close()
+
+#print(entry)
+
+n = int(entry[0][0])
+m = int(entry[0][1])
+
+# remove first line
+entry.pop(0)
+
+# Create Graph
+Graph = GraphVisualization()
+
+# Add Nodes
+for i in range(n):
+    Graph.addNode(Node(i+1))
+
+# Add Edges
+for i in range(m):
+    Node_a = int(entry[i][0])
+    Node_b = int(entry[i][1])
+    weight = int(entry[i][2])
+    a = Graph.nodes[Node_a-1]
+    b = Graph.nodes[Node_b-1]
+    Graph.addEdge(a,b,weight)
+
+for element in Graph.edges:
+    print(element)
+
+Graph.visualize()
+
+# MST 
+
+MST = Graph.Kruskal()
+
+MST.visualize()
